774831c62ee038ff8e986e2e749ac56b82220b6d
CLeandroDias <carlos.leandro@accenture.com>
2022-12-08 10:03:37 -0300

    Subida da estrutura inicial do projeto.

---
+++ src/main/java/br/acc/ProjetoBancarioGrupoUm/ProjetoBancarioGrupoUmApplication.java
@@ -0,0 +1,13 @@
+package br.acc.ProjetoBancarioGrupoUm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBancarioGrupoUmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBancarioGrupoUmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/acc/ProjetoBancarioGrupoUm/ProjetoBancarioGrupoUmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.acc.ProjetoBancarioGrupoUm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBancarioGrupoUmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
